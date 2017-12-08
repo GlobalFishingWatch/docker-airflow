f6906a4b3de72d2f4ece2fd271e57fdaf0e3fc61
bitsofbits <tim@bitsofbits.com>
2017-12-07 17:03:24 -0700

    use paul's docker fix

--- utils/install_dag.sh
+++ utils/install_dag.sh
@@ -17,12 +17,9 @@ echo "Creating $DEST_PATH as destination"
 # TODO: What to do if path exists?
 mkdir $DEST_PATH
 
-echo "Authorizing Docker"
-gcloud docker -a
-
 echo "Executing install.sh from docker_file"
 CONTAINER=gcr.io/world-fishing-827/$1
-docker run -v $HOST_DEST_PATH:/dags $CONTAINER /bin/bash install.sh
+gcloud docker -- run -v $HOST_DEST_PATH:/dags $CONTAINER /bin/bash install.sh
 
 POST_INSTALLER=$DEST_PATH/post_install.sh
 echo "Executing $POST_INSTALLER"

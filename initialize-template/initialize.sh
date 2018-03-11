11ab420d7bb9263d2ed2e10d9d2236c4d31cece3
Paul Woods <pwoods25443@gmail.com>
2018-03-11 15:23:08 -0400

    refactor test mode tools

--- initialize-template/initialize.sh
+++ initialize-template/initialize.sh
@@ -6,12 +6,10 @@ MOUNT_PATH=/usr/local/airflow/initialize
 
 
 # Install GCP Service Account
-#airflow connections --delete --conn_id=google_cloud_default
 airflow connections --add \
   --conn_id=google_cloud_default \
-  --conn_type=google_cloud_platform
-#  --conn_extra="$(cat ${MOUNT_PATH}/connection.json)"
-
+  --conn_type=google_cloud_platform \
+  --conn_extra="$(cat ${MOUNT_PATH}/connection.json)"
 
 #Install Variables
 airflow variables --import ${MOUNT_PATH}/variables.json
--- up.sh
+++ up.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+mkdir -p temp-dags
+rm -rf temp-dags/*
 docker-compose -f docker-compose-test.yaml up -d
 
